0ec3214c74fe80b55b6226107a042a49ec43fe8e
jake park <jake@parkjake.com>
2018-09-19 12:08:45 -0400

    integrate redux store

---
+++ src/reducers/timerReducer.js
@@ -0,0 +1,12 @@
+import { START_TIMER } from '../actions/types';
+
+const initialState = {
+
+}
+
+export default function(state = initialState, action){
+  switch(action.type) {
+    default: 
+      return state;
+  }
+}
