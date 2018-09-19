7b5defb0c061dd3760bbe9a051ff577e4a325633
jake park <jake@parkjake.com>
2018-09-19 00:35:27 -0400

    add timerForm

--- src/components/TimerForm.js
+++ src/components/TimerForm.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class TimerForm extends Component {
   render() {
     return (
-      <div>
-        
+      <div class="user-input">
+        <h1>Input Time:</h1>
+        <label for="">
+          Minute
+          <input type="number" oninput="this.value = Math.abs(this.value)" onkeyup="if(this.value > 15) this.value = 15;" min="0" max="15" id="input-minute"/>
+        </label>
+        <label for="">
+          Seconds
+          <input type="number" oninput="this.value = Math.abs(this.value)" onkeyup="if(this.value > 60) this.value = 60;" min="0" max="60" id="input-second"/>
+        </label>
+
+        <button type="submit" onclick="START_TIMER()" value="Click"/>
       </div>
     )
   }
